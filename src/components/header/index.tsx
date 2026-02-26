2d7eb5600760bb4573ac50db0381ad6adc87c7f9
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-02-26 16:35:13 -0300

    Componente Header finalizado

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,44 @@
+//imagem
+import logoImg from "../../assets/logo.svg";
+
+//link
+import { Link } from "react-router-dom";
+
+//icones
+import { FiUser, FiLogIn } from "react-icons/fi";
+
 export function Header() {
-    return (
-        <div>
-            <h1>Header</h1>
-        </div>
-    );
+  const signed = false;
+  const loadingAuth = false;
+
+  return (
+    <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4">
+      <header className="flex w-full items-center justify-between max-w-7xl px-4 mx-auto">
+        <Link to="/">
+          <img
+            src={logoImg}
+            alt="Logo do site"
+            title="Logo do site"
+            aria-label="Logo do site"
+          />
+        </Link>
+
+        {!loadingAuth && signed && (
+          <Link to="/dashboard">
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiUser size={22} color="#000" />
+            </div>
+          </Link>
+        )}
+
+        {!loadingAuth && !signed && (
+          <Link to="/login">
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiLogIn size={22} color="#000" />
+            </div>
+          </Link>
+        )}
+      </header>
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import { router } from './App'
+import { router } from "./App";
 
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
